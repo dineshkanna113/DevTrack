51f665d23503c1e6566cf8dbde09832676ce53fc
Dinesh Kanna <dineshkanna113@gmail.com>
2025-07-12 22:45:57 +0530

    Fix: day 11

--- devtrack-backend/models.py
+++ devtrack-backend/models.py
@@ -1,21 +1,20 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
-
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
 
 class Issue(Base):
     __tablename__ = "issues"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    status = Column(String, default="open", nullable=False)  # "open" or "closed"
-    label = Column(String, default="task", nullable=False)   # e.g., bug, feature
-    assigned_to = Column(String, default="unassigned", nullable=False)
-    owner_id = Column(Integer, nullable=False)  # 👤 user who created it
+    description = Column(String)
+    status = Column(String, default="open")
+    label = Column(String, default="task")
+    assigned_to = Column(String, default="unassigned")
+    owner_id = Column(Integer, ForeignKey("users.id"))  # ✅ Required
